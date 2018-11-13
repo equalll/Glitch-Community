0ebbcb6ee07c25524e29aeb456a520317bfb5c70
Glitch (community) <none>
2018-11-13 20:35:42 +0000

    🍹💨 Checkpoint ./src/presenters/includes/how-it-works.jsx:2/4300 ./src/presenters/pages/index.jsx:2/54 ./styles/includes/how-it-works.styl:2/40

--- src/presenters/includes/how-it-works.jsx
+++ src/presenters/includes/how-it-works.jsx
@@ -26,9 +26,6 @@ export default class HowItWorks extends React.Component {
     const free = "https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Ffree.svg?1499350845981";
     const play = "https://cdn.hyperdev.com/6ce807b5-7214-49d7-aadd-f11803bc35fd%2Fplay.svg";
     const illustration = "https://cdn.glitch.com/543c059e-33e3-4490-8582-b02ea65ed3a0%2Fwhat-is-glitch-simple.svg?1542137894293"
-    // const whatsGlitchWide = "https://cdn.glitch.com/f7224274-1330-4022-a8f2-8ae09dbd68a8%2Fwhats-glitch-wide.svg?1499885209761";
-    // const whatsGlitchNarrow = "https://cdn.glitch.com/f7224274-1330-4022-a8f2-8ae09dbd68a8%2Fwhats-glitch-narrow.svg?1499884900667";
-    // const whatsGlitchAlt = "Create a node app, or remix one. It updates as you type. Code with Friends!";
     return (
       <section className="how-it-works">
         <h2>How It Works</h2>
@@ -46,14 +43,13 @@ export default class HowItWorks extends React.Component {
           <p>
             Glitch is <img className="free" src={free} alt="free"/>
           </p>
-          <p>
-            <OverlayVideo>
-              <div className="video">
-                <img className="play-button" src={play} alt="play"/>
-                <span>How it works in 1 minute</span>
-              </div>
-            </OverlayVideo>
-          </p>
+          
+          <OverlayVideo>
+            <div className="video">
+              <img className="play-button" src={play} alt="play"/>
+              <span>How it works in 1 minute</span>
+            </div>
+          </OverlayVideo>
 
           <p>
             <Link to="/about/features">
@@ -66,7 +62,9 @@ export default class HowItWorks extends React.Component {
               About Teams
             </Link>
           </p>
-          <NewProjectPop api={api}/>
+
+          <NewProjectPop api={this.props.api}/>
+          
         </div>
       </section>
     );
